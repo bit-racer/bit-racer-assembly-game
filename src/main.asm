eede8eefc2921a0688029abc790ff44e25db136a
amir-kedis <amir.kedis@gmail.com>
2023-12-10 17:53:32 +0200

    Page 0 Complete

--- src/main.asm
+++ src/main.asm
@@ -68,6 +68,7 @@ DATA SEGMENT USE16
     currentColumn       db              0
     currentRow          db              0
     currentColor        db              BLACK
+    curCar              db              0                                     ; 0: red, 1: green, 2: reed, 3: pink
 
     ; User 1 data
                         usernameBuffer1 label byte
@@ -80,6 +81,9 @@ DATA SEGMENT USE16
     ptsActualSize1      db              ?
     pointsString1       db              5 DUP ('$')
 
+    usrCar1             db              0                                     ; 0: red, 1: green, 2: reed, 3: pink
+
+
     ; User 2 data
                         usernameBuffer2 label byte
     usrMaxSize2         db              16
@@ -90,6 +94,10 @@ DATA SEGMENT USE16
     ptsMaxSize2         db              4
     ptsActualSize2      db              ?
     pointsString2       db              5 DUP ('$')
+
+    usrCar2             db              0                                     ; 0: red, 1: green, 2: reed, 3: pink
+
+    
 DATA ENDS
 ;=================================================================================
 
@@ -97,99 +105,155 @@ DATA ENDS
 ; CODE SEGMENT
 
 CODE SEGMENT USE16
-                ASSUME          CS:CODE, DS:DATA
+                    ASSUME          CS:CODE, DS:DATA
 
     ;=================================================================================
     ; PROCEDURES INCLUDE
-                include         procs.inc
-                include         drawP.inc
+                    include         procs.inc
+                    include         drawP.inc
     ;=================================================================================
 
-    BEG:        
-                mov             ax, DATA
-                mov             ds, ax
+    BEG:            
+                    mov             ax, DATA
+                    mov             ds, ax
     
     ;=================================================================================
     ; Code Starts Here
     
-                SetVideoMode
-                ColorScreen     BG_COLOR
+                    SetVideoMode
+                    ColorScreen     BG_COLOR
 
     ; DRAW PAGE 0
-                DrawPage0
+                    DrawPage0
     
-                WaitForKeyPress
+                    WaitForKeyPress
 
-    
-
-                mov             currentColumn, 30
-                mov             currentRow, 16
-                call            moveCursor
+                    mov             currentColumn, 30
+                    mov             currentRow, 16
+                    call            moveCursor
     
     ; Read username1
-                readData        str_enter_usrname1, usernameBuffer1
+                    readData        str_enter_usrname1, usernameBuffer1
     
     ; Read points1
-                readData        str_initial_points1, pointsBuffer1
+                    readData        str_initial_points1, pointsBuffer1
     
+    ; Read usr car by moving by the arrow keys <- and -> to choose press enter
+                    MOV             curCar, 0
+                    CALL            DrawMarker
+    ChooseCar1:     
+                    MOV             AH, 0                                  ; wait for keypress from user: ah = scancode
+                    INT             16h
+                    CMP             AH, key_a                              ; left arrow
+                    JE              ChooseCar1Left
+                    CMP             AH, key_d                              ; right arrow
+                    JE              ChooseCar1Right
+                    CMP             AH, ENTER_KEY
+                    JE              ChooseCar1Done
+                    JMP             ChooseCar1
+    ChooseCar1Left: 
+                    CMP             curCar, 0
+                    JE              ChooseCar1
+                    DEC             curCar
+                    CALL            DrawMarker
+                    JMP             ChooseCar1
+    ChooseCar1Right:
+                    CMP             curCar, 3
+                    JE              ChooseCar1
+                    INC             curCar
+                    CALL            DrawMarker
+                    JMP             ChooseCar1
+    ChooseCar1Done: 
+                    MOV             AL, curCar
+                    MOV             usrCar1, AL
+                
 
 
     ;;;;;;;;;;;;;;;;;;;;;;;;; promt and wait for enter key
-                mov             dx, offset str_press_enter_key
-                call            printmsg
+                    mov             dx, offset str_press_enter_key
+                    call            printmsg
 
-    waiting:    
-                mov             ah, 0
-                int             16h                                    ; wait for keypress from user: ah = scancode
-                cmp             ah, ENTER_KEY
-                je              nextScreen
-                jmp             waiting
+    waiting:        
+                    mov             ah, 0
+                    int             16h                                    ; wait for keypress from user: ah = scancode
+                    cmp             ah, ENTER_KEY
+                    je              nextScreen
+                    jmp             waiting
 
 
-    nextScreen: 
+    nextScreen:     
     ;?;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;SCREEN SEPARATOR;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                ColorScreen     BG_COLOR
-                DrawPage0
-                mov             currentColumn, 30
-                mov             currentRow, 16
-                call            moveCursor
+                    ColorScreen     BG_COLOR
+                    DrawPage0
+                    mov             currentColumn, 30
+                    mov             currentRow, 16
+                    call            moveCursor
 
     ; Read username2
-                readData        str_enter_usrname2, usernameBuffer2
+                    readData        str_enter_usrname2, usernameBuffer2
 
     ; Read points1
-                readData        str_initial_points2, pointsBuffer2
+                    readData        str_initial_points2, pointsBuffer2
+    
+    ; Read usr car by moving by the arrow keys <- and -> to choose press enter
+                    MOV             curCar, 0
+                    CALL            DrawMarker
+    ChooseCar2:     
+                    MOV             AH, 0                                  ; wait for keypress from user: ah = scancode
+                    INT             16h
+                    CMP             AH, key_a                              ; left arrow
+                    JE              ChooseCar2Left
+                    CMP             AH, key_d                              ; right arrow
+                    JE              ChooseCar2Right
+                    CMP             AH, ENTER_KEY
+                    JE              ChooseCar2Done
+                    JMP             ChooseCar2
+    ChooseCar2Left: 
+                    CMP             curCar, 0
+                    JE              ChooseCar2
+                    DEC             curCar
+                    CALL            DrawMarker
+                    JMP             ChooseCar2
+    ChooseCar2Right:
+                    CMP             curCar, 3
+                    JE              ChooseCar2
+                    INC             curCar
+                    CALL            DrawMarker
+                    JMP             ChooseCar2
+    ChooseCar2Done: 
+                    MOV             AL, curCar
+                    MOV             usrCar2, AL
 
     ;;;;;;;;;;;;;;;;;;;;;;;;; promt and wait for enter key
-                mov             dx, offset str_press_enter_key
-                call            printmsg
+                    mov             dx, offset str_press_enter_key
+                    call            printmsg
 
-    waiting2:   
-                mov             ah, 0
-                int             16h                                    ; wait for keypress from user: ah = scancode
-                cmp             ah, ENTER_KEY
-                je              nextScreen2
-                jmp             waiting2
+    waiting2:       
+                    mov             ah, 0
+                    int             16h                                    ; wait for keypress from user: ah = scancode
+                    cmp             ah, ENTER_KEY
+                    je              nextScreen2
+                    jmp             waiting2
 
 
-    nextScreen2:
+    nextScreen2:    
     ;?;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;SCREEN SEPARATOR;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                ColorScreen     BG_COLOR
-                DrawPage0
-                mov             currentColumn, 30
-                mov             currentRow, 16
-                call            moveCursor
+                    ColorScreen     BG_COLOR
+                    DrawPage0
+                    mov             currentColumn, 30
+                    mov             currentRow, 16
+                    call            moveCursor
 
     ; print both data to make sure
-                printData       username1, pointsString1
-                printData       username2, pointsString2
+                    printData       username1, pointsString1
+                    printData       username2, pointsString2
 
-                WaitForKeyPress
+                    WaitForKeyPress
 
     ;;;;;;;;;;;;;;;;;;;;;;;;; End
-    byebye:     
-                mov             ax ,4c00h
-                int             21h
+    byebye:         
+                    mov             ax ,4c00h
+                    int             21h
 
 CODE ENDS
 ;=================================================================================
