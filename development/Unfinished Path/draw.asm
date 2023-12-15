5e3b2cfb58ddc5c49a744bd89817663d7dbc5cac
amir-kedis <amir.kedis@gmail.com>
2023-12-15 11:44:07 +0200

    Conrners not working (FIx in phase 2)

--- development/Unfinished Path/draw.asm
+++ development/Unfinished Path/draw.asm
@@ -22,7 +22,7 @@ DATA SEGMENT USE16
   ; Images and their meta data
 
   SEGMENT_SIZE    EQU     51
-                  include car1.inc
+                  include track.inc
   ;---------------------------------------------------------------------------------------------------------------------------
 
   
@@ -56,6 +56,7 @@ DATA SEGMENT USE16
   Where           db      0
   Prev_img        dw      0
   DIRECTIONS_DEMO DB      DIR_SIZE (?)
+  track_image     Dw      0
 
   ; DIRECTIONS_DEMO DB      3,3,1,2
   spare           db      20
@@ -67,85 +68,150 @@ DATA SEGMENT USE16
 DATA ENDS
 CODE SEGMENT USE16
 
-             ASSUME          CS:CODE,DS:DATA
+              ASSUME          CS:CODE,DS:DATA
 
   ; --------------------------------------------------------------------------------------------------------------------------------
   ; DRAW Procedures
 
          
-             include         PROCEEDS.inc
+              include         PROCEEDS.inc
        
   ; --------------------------------------------------------------------------------------------------------------------------------
  
 
  
-  BEG:       
-             MOV             AX,DATA
-             MOV             DS,AX
+  BEG:        
+              MOV             AX,DATA
+              MOV             DS,AX
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   ; Set Video Mode
-             SetVideoMode
+              SetVideoMode
 
   ; Color The Screen
-             ColorScreen     0
+              ColorScreen     0
 
    
   ; mov          di,offset img1F
   ; DrawCar      [car1_X_Offset],[car1_Y_Offset],car1V_Width,car1V_Height
   ; mov          di,offset img2F
   ; DrawCar      [car2_X_Offset],[car2_Y_Offset],car2V_Width,car2V_Height
-  MAINLOOP2: 
+  MAINLOOP2:  
 
 
-             ClearScreen
+              ClearScreen
   ; reset
-             MOV             COUNT,0
-             MOV             DIRECTION,0
+              MOV             COUNT,0
+              MOV             DIRECTION,0
+  ; NOTE: remove following 2 lines if you want random start
+              MOV             car1_X_Offset,0
+              MOV             car1_Y_Offset,0
   ;MOV             SEED,0
-             MOV             DELAY,10000
+              MOV             DELAY,10000
 
-             CALL            GENERATE_NEW_COMBINATION
-             MOV             BX,OFFSET DIRECTIONS_DEMO
-             MOV             SI,0000H
+              CALL            GENERATE_NEW_COMBINATION
+              MOV             BX,OFFSET DIRECTIONS_DEMO
+              MOV             SI,0000H
 
-  DRAW_TRACK:
-             INC             SI
-             CMP             BYTE PTR [BX],0
-             JE              ADDTOUP
-             CMP             BYTE PTR    [BX],1
-             JE              ADDTODOWN
-             CMP             BYTE PTR [BX],2
-             JE              ADDTOLEFT
-             CMP             BYTE PTR    [BX],3
-             JE              ADDTORIGHT
+  DRAW_TRACK: 
+              INC             SI
+              CMP             BYTE PTR [BX],0
+              JE              ADDTOUP
+              CMP             BYTE PTR    [BX],1
+              JE              ADDTODOWN
+              CMP             BYTE PTR [BX],2
+              JE              ADDTOLEFT
+              CMP             BYTE PTR    [BX],3
+              JE              ADDTORIGHT
                              
-  ADDTOUP:   
-             PUSH            BX
-             call            moveUp
-             POP             BX
-             JMP             CHECKING
-  ADDTODOWN: 
-             PUSH            BX
-             call            moveDown
-             POP             BX
-             JMP             CHECKING
-  ADDTOLEFT: 
-             PUSH            BX
-             call            moveLeft
-             POP             BX
-             JMP             CHECKING
-  ADDTORIGHT:
-             PUSH            BX
-             call            moveRight
-             POP             BX
-             JMP             CHECKING
-  CHECKING:  
-             INC             BX
-             CMP             SI,DIR_SIZE
-             JNZ             DRAW_TRACK
-             CMP             COUNT,SEGMENT_COUNT
-             JB              MAINLOOP2
+  ADDTOUP:    
+              PUSH            BX
+  ; Check next direction to put coreners
+
+              CMP             BYTE PTR [BX+1],D_UP
+              JE              UP_UP
+              CMP             BYTE PTR [BX+1],D_LEFT
+              JE              UP_LEFT
+              CMP             BYTE PTR [BX+1],D_RIGHT
+              JE              UP_RIGHT
+              JMP             UP_UP
+  UP_UP:      MOV             track_image, offset img_ts_ud
+              JMP             UP_MOVE
+  UP_LEFT:    MOV             track_image, offset img_ts_c2bl
+              JMP             UP_MOVE
+  UP_RIGHT:   MOV             track_image, offset img_ts_c1br
+              JMP             UP_MOVE
+  UP_MOVE:    
+              call            moveUp
+              POP             BX
+              JMP             CHECKING
+  ADDTODOWN:  
+              PUSH            BX
+  ; Check next direction to put coreners
+              CMP             BYTE PTR [BX+1],D_DOWN
+              JE              DOWN_DOWN
+              CMP             BYTE PTR [BX+1],D_LEFT
+              JE              DOWN_LEFT
+              CMP             BYTE PTR [BX+1],D_RIGHT
+              JE              DOWN_RIGHT
+              JMP             DOWN_DOWN
+  DOWN_DOWN:  MOV             track_image, offset img_ts_ud
+              JMP             DOWN_MOVE
+  DOWN_LEFT:  MOV             track_image, offset img_ts_c3tl
+              JMP             DOWN_MOVE
+  DOWN_RIGHT: MOV             track_image, offset img_ts_c4tr
+              JMP             DOWN_MOVE
+  DOWN_MOVE:  
+
+              call            moveDown
+              POP             BX
+              JMP             CHECKING
+  ADDTOLEFT:  
+              PUSH            BX
+  ; Check next direction to put coreners
+              CMP             BYTE PTR [BX+1],D_UP
+              JE              LEFT_UP
+              CMP             BYTE PTR [BX+1],D_DOWN
+              JE              LEFT_DOWN
+              CMP             BYTE PTR [BX+1],D_LEFT
+              JE              LEFT_LEFT
+              JMP             LEFT_LEFT
+  LEFT_UP:    MOV             track_image, offset img_ts_c3tl
+              JMP             LEFT_MOVE
+  LEFT_DOWN:  MOV             track_image, offset img_ts_c2bl
+              JMP             LEFT_MOVE
+  LEFT_LEFT:  MOV             track_image, offset img_ts_lr
+              JMP             LEFT_MOVE
+  LEFT_MOVE:  
+              call            moveLeft
+              POP             BX
+              JMP             CHECKING
+  ADDTORIGHT: 
+              PUSH            BX
+  ; Check next direction to put coreners
+              CMP             BYTE PTR [BX+1],D_UP
+              JE              RIGHT_UP
+              CMP             BYTE PTR [BX+1],D_DOWN
+              JE              RIGHT_DOWN
+              CMP             BYTE PTR [BX+1],D_RIGHT
+              JE              RIGHT_RIGHT
+              JMP             RIGHT_RIGHT
+  RIGHT_UP:   MOV             track_image, offset img_ts_c3tl
+              JMP             RIGHT_MOVE
+  RIGHT_DOWN: MOV             track_image, offset img_ts_c1br
+              JMP             RIGHT_MOVE
+  RIGHT_RIGHT:MOV             track_image, offset img_ts_lr
+              JMP             RIGHT_MOVE
+  RIGHT_MOVE: 
+              call            moveRight
+              POP             BX
+              JMP             CHECKING
+  CHECKING:   
+              INC             BX
+              CMP             SI,DIR_SIZE
+              JNZ             DRAW_TRACK
+              CMP             COUNT,SEGMENT_COUNT
+              JB              MAINLOOP2
   ;get                the address of the existing int09h handler
   ;      mov          ax, 3509h                                                    ; Get Interrupt Vector
   ;      int          21h                                                          ; -> ES:BX
@@ -170,11 +236,11 @@ CODE SEGMENT USE16
   ;      pop          dx ds
   ;      mov          ax, 2509h
   ;      int          21h
-             WaitForKeyPress
-             JMP             MAINLOOP2
-  exit:      
-             MOV             AH,4CH
-             INT             21H
+              WaitForKeyPress
+              JMP             MAINLOOP2
+  exit:       
+              MOV             AH,4CH
+              INT             21H
 
 CODE ENDS
 END BEG
