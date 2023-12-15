75e38b153a24206f2c629d478fe9cff7db66cd8b
amir-kedis <amir.kedis@gmail.com>
2023-12-15 11:58:33 +0200

    Clean draw.asm

--- development/Unfinished Path/draw.asm
+++ development/Unfinished Path/draw.asm
@@ -21,48 +21,48 @@ DATA SEGMENT USE16
   ;---------------------------------------------------------------------------------------------------------------------------
   ; Images and their meta data
 
-                  SEGMENT_SIZE EQU     51
-                  include      track.inc
+  SEGMENT_SIZE    EQU     51
+                  include track.inc
   ;---------------------------------------------------------------------------------------------------------------------------
 
   
   ; SCREEN INFO
-  VIDEO_MODE      EQU          4F02h         ; SVGA MODE
-  VIDEO_MODE_BX   EQU          0101h         ; SCREEN SIZES
+  VIDEO_MODE      EQU     4F02h         ; SVGA MODE
+  VIDEO_MODE_BX   EQU     0101h         ; SCREEN SIZES
 
-  SCREEN_WIDTH    EQU          640
-  SCREEN_HEIGHT   EQU          480
-  SEGMENT_COUNT   EQU          40
-  DIR_SIZE        EQU          200
-  DIR             DB           0
+  SCREEN_WIDTH    EQU     640
+  SCREEN_HEIGHT   EQU     480
+  SEGMENT_COUNT   EQU     40
+  DIR_SIZE        EQU     200
+  DIR             DB      0
 
   ; DIRECTIONS
-  D_UP            EQU          0
-  D_DOWN          EQU          1
-  D_LEFT          EQU          2
-  D_RIGHT         EQU          3
+  D_UP            EQU     0
+  D_DOWN          EQU     1
+  D_LEFT          EQU     2
+  D_RIGHT         EQU     3
   ; --------------------------------------------------------------------------------------------------------------------------------
   ; DRAW PARAMETERS
-  IMAGE_OFFSET_X  dw           0
-  IMAGE_OFFSET_Y  dw           0
-  IMAGE_SIZE_X    dw           0
-  IMAGE_SIZE_Y    dw           0
-  REVERSE         DB           0
-  ERASE           DB           0
-  RECOLOR         DB           0
-  COUNT           DB           0
+  IMAGE_OFFSET_X  dw      0
+  IMAGE_OFFSET_Y  dw      0
+  IMAGE_SIZE_X    dw      0
+  IMAGE_SIZE_Y    dw      0
+  REVERSE         DB      0
+  ERASE           DB      0
+  RECOLOR         DB      0
+  COUNT           DB      0
   ;---------------------------------------------------------------------------------------------------------------------------------
-  KeyList         db           128 dup (0)
-  Where           db           0
-  Prev_img        dw           0
-  DIRECTIONS_DEMO DB           DIR_SIZE (?)
+  KeyList         db      128 dup (0)
+  Where           db      0
+  Prev_img        dw      0
+  DIRECTIONS_DEMO DB      DIR_SIZE (?)
 
   ; DIRECTIONS_DEMO DB      3,3,1,2
-  spare           db           20
-  DELAY           DW           10000
+  spare           db      20
+  DELAY           DW      10000
   
-  DIRECTION       DB           ?
-  SEED            DB           ?
+  DIRECTION       DB      ?
+  SEED            DB      ?
 
 DATA ENDS
 CODE SEGMENT USE16
@@ -71,8 +71,6 @@ CODE SEGMENT USE16
 
   ; --------------------------------------------------------------------------------------------------------------------------------
   ; DRAW Procedures
-
-         
              include         PROCEEDS.inc
        
   ; --------------------------------------------------------------------------------------------------------------------------------
@@ -90,11 +88,6 @@ CODE SEGMENT USE16
   ; Color The Screen
              ColorScreen     0
 
-   
-  ; mov          di,offset img1F
-  ; DrawCar      [car1_X_Offset],[car1_Y_Offset],car1V_Width,car1V_Height
-  ; mov          di,offset img2F
-  ; DrawCar      [car2_X_Offset],[car2_Y_Offset],car2V_Width,car2V_Height
   MAINLOOP2: 
 
 
@@ -102,7 +95,6 @@ CODE SEGMENT USE16
   ; reset
              MOV             COUNT,0
              MOV             DIRECTION,0
-  ;MOV             SEED,0
              MOV             DELAY,10000
 
              CALL            GENERATE_NEW_COMBINATION
@@ -146,34 +138,11 @@ CODE SEGMENT USE16
              JNZ             DRAW_TRACK
              CMP             COUNT,SEGMENT_COUNT
              JB              MAINLOOP2
-  ;get                the address of the existing int09h handler
-  ;      mov          ax, 3509h                                                    ; Get Interrupt Vector
-  ;      int          21h                                                          ; -> ES:BX
-  ;      push         es bx
-
-  ; ; replace the existing int09h handler with ours
-  ;      mov          dx, offset onKeyEvent
-
-  ;      MOV          AX,CS
-  ;      MOV          DS,AX
-  ;      mov          ax, 2509h
-  ;      int          21h
-  ;      POP          DS
-  ;      call         main
-
-  ; ; return to text mode
-  ;      mov          ah, 0
-  ;      mov          al, 2
-  ;      int          10h
-
-  ; ; restore the original int09h handler
-  ;      pop          dx ds
-  ;      mov          ax, 2509h
-  ;      int          21h
+ 
              WaitForKeyPress
-             ;if escape pressed exit
-              CMP             AL,1Bh
-              JE              exit
+  ;if escape pressed exit
+             CMP             AL,1Bh
+             JE              exit
              JMP             MAINLOOP2
   exit:      
              MOV             AH,4CH
